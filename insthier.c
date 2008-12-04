b4189d0e66fa5e2e86cfede71f388f39f6d860e8
m0 <m0@localhost>
2008-12-04 03:09:29 +0000

    regenerated insthier.

--- insthier.c
+++ insthier.c
@@ -12,6 +12,8 @@ struct install_item insthier[] = {
   {INST_COPY, "sdl-mixer.ads", 0, ctxt_incdir, 0, 0, 0644},
   {INST_COPY, "sdl-mixer.adb", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "sdl-mixer.adb", 0, ctxt_incdir, 0, 0, 0644},
+  {INST_COPY, "sdl-mixer.ali", 0, ctxt_repos, 0, 0, 0644},
+  {INST_COPY, "sdl-mixer.ali", 0, ctxt_incdir, 0, 0, 0444},
   {INST_COPY, "sdl-mix-ada.sld", 0, ctxt_repos, 0, 0, 0644},
   {INST_COPY, "sdl-mix-ada.a", "libsdl-mix-ada.a", ctxt_slibdir, 0, 0, 0644},
   {INST_COPY, "sdl-mix-ada-conf.ld", 0, ctxt_repos, 0, 0, 0644},
